51792208bd1dd4224e48bdb8b82d9c6d8723ad13
Marius Kruger <amanic@gmail.com>
2023-06-11 01:00:56 +0200

    Starter project generated with https://start.spring.io
    * https://start.spring.io/#!type=gradle-project&language=java&platformVersion=3.1.0&packaging=jar&jvmVersion=20&groupId=com.example&artifactId=vaadin-spring-poc&name=vaadin-spring-poc&description=Vaadin%20project%20for%20Spring%20Boot&packageName=com.example.vaadinspringpoc&dependencies=vaadin,lombok,devtools,configuration-processor,data-jpa,h2,validation,actuator

---
+++ src/main/java/com/example/vaadinspringpoc/VaadinSpringPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaadinspringpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinSpringPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinSpringPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaadinspringpoc/VaadinSpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaadinspringpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinSpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
