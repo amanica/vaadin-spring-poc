07686be19ddb08c5f22bd2acd71068558a4a870a
Marius Kruger <amanic@gmail.com>
2023-06-14 22:49:23 +0200

    clean up test a little

--- src/test/java/com/example/vaadinspringpoc/data/service/GameServiceTest.java
+++ src/test/java/com/example/vaadinspringpoc/data/service/GameServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -28,34 +29,23 @@ class GameServiceTest {
     GameService gameService;
     @Autowired
     PlayerService playerService;
-
-    Player player1;
-    Player player2;
-    Player player5;
-
-    int player1StartRank;
-    int player2StartRank;
-    int player5StartRank;
+    
+    List<Player> origPlayers;
+    List<Integer> origRanks;
 
     @BeforeEach
     void setUp() {
-        List<Player> origPlayers = playerService.findAllPlayersOrderByRank();
+        origPlayers = playerService.findAllPlayersOrderByRank();
         assumeThat(origPlayers, hasSize(greaterThanOrEqualTo(6)));
-        player1 = origPlayers.get(1);
-        player2 = origPlayers.get(2);
-        player5 = origPlayers.get(5);
-
-        player1StartRank = player1.getCurrentRank();
-        player2StartRank = player2.getCurrentRank();
-        player5StartRank = player5.getCurrentRank();
+        origRanks = origPlayers.stream().map(Player::getCurrentRank).collect(Collectors.toList());
     }
 
     @Test
     void saveGame_givenNewGameWithSamePlayer() {
         //given
         Game game = new Game();
-        game.setWhitePlayer(player1);
-        game.setBlackPlayer(player1);
+        game.setWhitePlayer(origPlayers.get(1));
+        game.setBlackPlayer(origPlayers.get(1));
 
         //when
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
@@ -70,8 +60,8 @@ class GameServiceTest {
     void saveGame_givenNewGame_withNullResult() {
         //given
         Game game = new Game();
-        game.setWhitePlayer(player1);
-        game.setBlackPlayer(player2);
+        game.setWhitePlayer(origPlayers.get(1));
+        game.setBlackPlayer(origPlayers.get(2));
 //        game.setResult(GameResult.DRAW);
 
         //when
@@ -86,57 +76,60 @@ class GameServiceTest {
     void saveGame_givenNewGame_withLowerRankWinning_thenRanksDontChange() {
         //given
         Game game = new Game();
-        game.setWhitePlayer(player1);
-        game.setBlackPlayer(player5);
+        game.setWhitePlayer(origPlayers.get(1));
+        game.setBlackPlayer(origPlayers.get(5));
         game.setResult(GameResult.BLACK_WIN);
         long origGameCount = gameService.countGames();
 
         //when
         gameService.saveGame(game);
 
-        //then
+        //then the came persisted and retrieved correctly
         assertThat(gameService.countGames(), equalTo(origGameCount + 1));
         Game newGame = gameService.findAllGamesOrderByDateDesc().get(0);
-        assertThat(newGame.getWhitePlayer(), equalTo(player1));
-        assertThat(newGame.getBlackPlayer(), equalTo(player5));
-        assertThat(newGame.getWhiteStartRank(), equalTo(player1StartRank));
-        assertThat(newGame.getBlackStartRank(), equalTo(player5StartRank));
+        assertThat(newGame.getWhitePlayer(), equalTo(origPlayers.get(1)));
+        assertThat(newGame.getBlackPlayer(), equalTo(origPlayers.get(5)));
+        assertThat(newGame.getWhiteStartRank(), equalTo(origRanks.get(1)));
+        assertThat(newGame.getBlackStartRank(), equalTo(origRanks.get(5)));
 
-        //assert ranks didn't change
-        assertThat(getNewRank(player1), equalTo(player1StartRank));
-        assertThat(getNewRank(player5), equalTo(player5StartRank));
+        //and ranks didn't change
+        assertThat(getNewRank(origPlayers.get(1)), equalTo(origRanks.get(1)));
+        assertThat(getNewRank(origPlayers.get(5)), equalTo(origRanks.get(5)));
     }
 
     @Test
     void saveGame_givenNewGame_withAdjacentDraw_thenRanksDontChange() {
         //given
         Game game = new Game();
-        game.setWhitePlayer(player1);
-        game.setBlackPlayer(player2);
+        game.setWhitePlayer(origPlayers.get(1));
+        game.setBlackPlayer(origPlayers.get(2));
         game.setResult(GameResult.DRAW);
 
         //when
         gameService.saveGame(game);
 
         //then ranks didn't change
-        assertThat(getNewRank(player1), equalTo(player1StartRank));
-        assertThat(getNewRank(player2), equalTo(player2StartRank));
+        assertThat(getNewRank(origPlayers.get(1)), equalTo(origRanks.get(1)));
+        assertThat(getNewRank(origPlayers.get(2)), equalTo(origRanks.get(2)));
     }
 
     @Test
-    void saveGame_givenNewGame_withDraw_thenBlackLowerRankMovesUp() {
+    void saveGame_givenNewGame_withDraw_thenBlackLowerRankMovesUpOne() {
         //given
         Game game = new Game();
-        game.setWhitePlayer(player1);
-        game.setBlackPlayer(player5);
+        game.setWhitePlayer(origPlayers.get(1));
+        game.setBlackPlayer(origPlayers.get(5)); // black has lower rank
         game.setResult(GameResult.DRAW);
 
         //when
         gameService.saveGame(game);
 
-        //then ranks didn't change
-        assertThat(getNewRank(player1), equalTo(player1StartRank));
-        assertThat(getNewRank(player5), equalTo(player5StartRank - 1));
+        //then white rank didn't change
+        assertThat(getNewRank(origPlayers.get(1)), equalTo(origRanks.get(1)));
+        //and player above black moves down one
+        assertThat(getNewRank(origPlayers.get(4)), equalTo(origRanks.get(4) + 1));
+        //and black move up one
+        assertThat(getNewRank(origPlayers.get(5)), equalTo(origRanks.get(5) - 1));
     }
 
     private Integer getNewRank(Player player) {
